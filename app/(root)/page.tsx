5df948b774b876fe7bca7d2159eb9c7beb2c2258
Cornelius Ezeh <106745710+tushydan007@users.noreply.github.com>
2025-06-05 16:48:51 +0100

    refactor code

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
-import Card from "@/components/shared/Card";
 import Hero from "../_components/Hero";
-import jpg from "@/public/assets/carousel1.jpg";
+
 import WhyPwan from "../_components/WhyPwan";
 import RightProgramme from "../_components/RightProgramme";
 
