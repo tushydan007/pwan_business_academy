aa33ef2fd7cc27ee03f3886231067cad226d9355
Cornelius Ezeh <106745710+tushydan007@users.noreply.github.com>
2025-06-18 17:52:09 +0100

    Refactor code

--- app/_components/WhyPwan.tsx
+++ app/_components/WhyPwan.tsx
@@ -6,12 +6,17 @@ const WhyPwan = () => {
       <h4 className="font-bold lg:text-4xl md:text-3xl text-2xl text-red-600">
         Why PWAN Business Academy?
       </h4>
-      <p className="mt-4 text-lg md:text-xl text-black">
+      <p className="mt-4 text-base md:text-lg text-black">
         PWAN Business Academy stands out as a premier institution for executive
         development, offering cutting-edge programs that blend world-class
         expertise with real-world business challenges. Our immersive learning
         approach empowers leaders with strategic thinking, innovation, and
-        leadership skills tailored to today&apos;s dynamic global marketplace
+        leadership skills tailored to today&apos;s dynamic global marketplace.At
+        PWAN Business Academy, we&apos;re more than just an institution,
+        we&apos;re a launchpad for your business and entrepreneurial success.
+        Our mission is to empower aspiring business owners, young professionals,
+        and ambitious students with practical knowledge, digital tools, and the
+        mindset needed to thrive in today&apos;s fast-paced economy.
       </p>
     </section>
   );
